21a0bbf8b3ef58284d9e0c2072828c1962038d11
Anastasia Bereznevich <nastena.bereznevich@mail.ru>
2022-09-17 16:17:40 +0300

    task 5

---
+++ Hometasks/standart_library_les_19/task_5.py
@@ -0,0 +1,17 @@
+"""
+Напишите функцию для создания строки HTML с тегами вокруг слов (использовать шаблону из модуля string).
+Пример функции и результата:
+add_tags('i', 'Python') -> '<i>Python</i>'
+add_tags('b', 'Python Tutorial') -> '<b>Python Tutorial </b>'
+"""
+
+
+from string import Template
+
+
+def add_tags(tag, message):
+    return Template("${name}" + message + "${name_1}").substitute(name=f'<{tag}>', name_1=f'</{tag}>')
+
+
+print(add_tags('i', 'hi'))
+print(add_tags('b', 'My friend'))
