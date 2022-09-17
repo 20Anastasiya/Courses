6ac74fbc739826dd85c14bc8abd251d7a5afca5c
Anastasia Bereznevich <nastena.bereznevich@mail.ru>
2022-09-17 14:40:36 +0300

    4 commit(with package)

--- Hometasks/modules/my_client.py
+++ Hometasks/modules/my_client.py
@@ -1,12 +1,22 @@
 """
 3. Вложенные операции импортирования. Создайте второй модуль, my_client.py,
 который импортирует модуль my_mod и тестирует его функции.
+
+4. Операции импортирования пакетов. Импортируйте свой файл из пакета. Создайте подкаталог по имени my_pkg внутри
+ рабочего каталога, переместите в новый подкаталог файл модуля my_mod.py, созданный в упражнении 1,
+  и попробуйте импортировать его с помощью операции импортирования пакета в форме import mypkg.mymod,
+  после чего вызовите функции модуля. Попробуйте извлечь функции подсчета и посредством from тоже.
 """
 
+# import my_mod
+# from Hometasks.modules.my_pkg import my_mod
+# import my_pkg.my_mod  #не работает
+# import Hometasks.modules.my_pkg.my_mod #не работает
+from my_pkg.my_mod import count_line
+from my_pkg.my_mod import count_chars
+from my_pkg.my_mod import test
 
-import my_mod
 
-
-print(my_mod.count_line('text.txt'))
-print(my_mod.count_chars('text.txt'))
-print(my_mod.test('text.txt'))
+print(count_line('text.txt'))
+print(count_chars('text.txt'))
+print(test('text.txt'))
