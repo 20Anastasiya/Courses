b52ccdc12e889d0ee5f506cf4414720f35988ed5
Anastasia Bereznevich <nastena.bereznevich@mail.ru>
2022-09-20 21:49:09 +0300

    add if __name__ task_6

--- Hometasks/standart_library_les_19/task_6.py
+++ Hometasks/standart_library_les_19/task_6.py
@@ -14,42 +14,43 @@
 import datetime
 
 
-def date_time():
-    return f'Дата и время: {datetime.datetime.now()}'
+if __name__ == '__main__':
+    def date_time():
+        return f'Дата и время: {datetime.datetime.now()}'
 
 
-def year():
-    return f'Год: {datetime.datetime.now().year}'
+    def year():
+        return f'Год: {datetime.datetime.now().year}'
 
 
-def month():
-    _now = datetime.datetime.now()
-    return f'Месяц: {datetime.datetime.now().month} ({_now.strftime("%B")})'
+    def month():
+        _now = datetime.datetime.now()
+        return f'Месяц: {datetime.datetime.now().month} ({_now.strftime("%B")})'
 
 
-def week_num():
-    _now = datetime.datetime.now()
-    return f'Номер недели в году: {_now.strftime("%W")}'
+    def week_num():
+        _now = datetime.datetime.now()
+        return f'Номер недели в году: {_now.strftime("%W")}'
 
 
-def weekday():
-    _now = datetime.datetime.now()
-    return f'Рабочий день недели: {_now.strftime("%u")}'
+    def weekday():
+        _now = datetime.datetime.now()
+        return f'Рабочий день недели: {_now.strftime("%u")}'
 
 
-def day_of_year():
-    _now = datetime.datetime.now()
-    return f'Номер дня в году: {_now.strftime("%j")}'
+    def day_of_year():
+        _now = datetime.datetime.now()
+        return f'Номер дня в году: {_now.strftime("%j")}'
 
 
-def day_of_month():
-    _now = datetime.datetime.now()
-    return f'Номер дня в месяце: {_now.strftime("%d")}'
+    def day_of_month():
+        _now = datetime.datetime.now()
+        return f'Номер дня в месяце: {_now.strftime("%d")}'
 
 
-def day_of_week():
-    _now = datetime.datetime.now()
-    return f'День недели: {_now.strftime("%A")}'
+    def day_of_week():
+        _now = datetime.datetime.now()
+        return f'День недели: {_now.strftime("%A")}'
 
 
-print(weekday())
+    print(weekday())
