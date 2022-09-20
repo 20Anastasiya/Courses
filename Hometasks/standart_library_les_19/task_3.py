53d6a19e3826fee95a24023d71f05f28e02aa35b
Anastasia Bereznevich <nastena.bereznevich@mail.ru>
2022-09-20 21:48:31 +0300

    add if __name__ task_3

--- Hometasks/standart_library_les_19/task_3.py
+++ Hometasks/standart_library_les_19/task_3.py
@@ -6,10 +6,11 @@
 from random import randint
 
 
-dice_1 = randint(1, 6)
-dice_2 = randint(1, 6)
+if __name__ == '__main__':
+    dice_1 = randint(1, 6)
+    dice_2 = randint(1, 6)
 
-print('Ваш результат: ', dice_1 + dice_2)
+    print('Ваш результат: ', dice_1 + dice_2)
 
-if dice_1 == dice_2:
-    print('Дубль!')
+    if dice_1 == dice_2:
+        print('Дубль!')
